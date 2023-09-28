284a0f43ca8fb9d765106285ce90616f2a628d66
Kelvin Hong <kh.boon2@gmail.com>
2023-09-28 10:36:40 +0800

    CPP training implemented, next is model saving and parallel computing

--- tests/regressionTest.cpp
+++ tests/regressionTest.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <fstream>
 #include <memory>
+#include <unordered_map>
 #include <numeric>
 #include <algorithm>
 
@@ -113,9 +114,15 @@ class DataLoader1D
             length(static_cast<int>(dataset[0].rows())), counter(0),
             perm(std::vector<int>(length))
         {
-            std::iota(perm.begin(), perm.end(), 0);
-            std::shuffle(perm.begin(), perm.end(), Deep::gen);
+            resetPermutation();
         };
+
+        void resetPermutation()
+        {
+            std::iota(perm.begin(), perm.end(), 0);
+            if (shuffle)
+                std::shuffle(perm.begin(), perm.end(), Deep::gen);
+        }
 
         std::vector<Eigen::MatrixXd> nextBatch()
         {
@@ -143,7 +150,7 @@ class DataLoader1D
         void reInitialize()
         {
             // Refresh the dataloader into its initial state, ready to resample again.
-            std::shuffle(perm.begin(), perm.end(), Deep::gen);
+            resetPermutation();
             counter = 0;
         }
         
@@ -170,13 +177,12 @@ int testForward()
     return 0;
 }
 
-int train(std::vector<Eigen::MatrixXd> dataset)
+void train(MyReg &model, std::vector<Eigen::MatrixXd> dataset)
 {
-    const int epochs { 50 };
+    const int epochs { 5 };
     const int bs { 64 };
 
-    MyReg model {};
-    Deep::Optim::SGD optimizer(model.namedParameters());
+    Deep::Optim::SGD optimizer(model.namedParameters(), 5e-5);
     DataLoader1D dl(dataset, bs, true);
 
     for (int epoch = 1; epoch <= epochs; ++epoch)
@@ -196,21 +202,54 @@ int train(std::vector<Eigen::MatrixXd> dataset)
             NSP LPtr { Deep::MSE(yPtr, trainLabel) } ;
             LPtr->backward();
             optimizer.step();
-            runningLoss += LPtr->data(0,0) * bs / currBatchSize ;
+            runningLoss += LPtr->data(0,0) * currBatchSize ;
         }
         double epochLoss {runningLoss / dl.length};
         std::cout << "\tLoss is " << epochLoss << ".\n";
         dl.reInitialize();
     }
 
-    return 0;
 }
+
+std::unordered_map<std::string, double> test(MyReg &model, std::vector<Eigen::MatrixXd> dataset)
+{
+    const int bs { 64 };
+    DataLoader1D  dl(dataset, bs, false);
+
+    int total { 0 };
+    int correct { 0 };
+    while (dl.hasNext())
+    {
+        std::vector<Eigen::MatrixXd> thisBatch {dl.nextBatch()};
+        Eigen::MatrixXd testFeature { thisBatch[0] };
+        Eigen::MatrixXd testLabel { thisBatch[1] };
+        NSP testNode { std::make_shared<Deep::Node>(testFeature) };
+        NSP yPtr { model.forward(testNode) };
+        // Round yPtr then compare with int labels.
+        Eigen::MatrixXd prediction { postProcess(yPtr) };
+        Eigen::ArrayXd diff {(prediction - testLabel).cwiseAbs().array()};
+        for (double num: diff)
+        {
+            ++total;
+            if (num < 0.5)
+                ++correct;
+        }
+    }
+    std::unordered_map<std::string, double> ret {{"accuracy", 100.0 * static_cast<double>(correct)/total }};
+    return ret;
+}
+
+
 
 int main()
 {   
     testForward();
 
+    MyReg model {};
     std::vector<Eigen::MatrixXd> dataset{loadData("./datasets/winequality/winequality-white-train.csv")};
-    train(dataset);
+    train(model, dataset);
+    std::vector<Eigen::MatrixXd> testDataset{loadData("./datasets/winequality/winequality-white-test.csv")};
+    std::unordered_map<std::string, double> result { test(model, testDataset) };
+    std::cout << "Accuracy is " <<  result["accuracy"] << "%.\n";
     return 0;
 }
